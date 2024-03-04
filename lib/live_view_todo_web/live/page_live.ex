02adf5b494d0686bc2282a7baf97f4aef347558d
boldPro <probold777@gmail.com>
2024-03-04 12:21:54 -0500

    listing items from db on the page

--- lib/live_view_todo_web/live/page_live.ex
+++ lib/live_view_todo_web/live/page_live.ex
@@ -3,22 +3,32 @@ defmodule LiveViewTodoWeb.PageLive do
   This module is implementation of PageLive in router
   """
   use LiveViewTodoWeb, :live_view
+
   alias LiveViewTodo.Item
+
 
   @impl true
   @topic "live"
+
+
+
   def mount(_params, _session, socket) do
     # For each LiveView in the root of a template, mount/3 is invoked twice: once to do the initial page load and again to establish the live socket.
     #After declaring the mount function we need to let the app know that we are going to use it
     # our app know which html file to render by it's name the same name as this module with the extention .html.heex
-    {:ok, socket}# socket is a liveView Socket that passes the endpoint for liveView todo web
+
+    # subscribe to the channel
+    if connected?(socket), do: LiveViewTodoWeb.Endpoint.subscribe(@topic)
+    {:ok, assign(socket, items: Item.list_items(), editing: nil, tab: "all")}# socket is a liveView Socket that passes the endpoint for liveView todo web
   end
 
   @impl true
   def handle_event("create", %{"text" => text}, socket) do
     Item.create_item(%{text: text})
+    IO.puts("CREATING")
     socket = assign(socket, items: Item.list_items(), active: %Item{})
     LiveViewTodoWeb.Endpoint.broadcast_from(self(), @topic, "update", socket.assigns)
+    IO.inspect(socket.items, label: "ITEMS in SOCKET")
     {:noreply, socket}
   end
 
@@ -26,5 +36,15 @@ defmodule LiveViewTodoWeb.PageLive do
   @impl true
   def handle_info(%{event: "update", payload: %{items: items}}, socket) do
     {:noreply, assign(socket, items: items)}
+  end
+
+
+
+  def checked?(item) do
+    not is_nil(item.status) and item.status > 0
+  end
+
+  def completed?(item) do
+    if not is_nil(item.status) and item.status > 0, do: "completed", else: ""
   end
 end
