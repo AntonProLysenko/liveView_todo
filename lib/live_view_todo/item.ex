b07f04a69c158b792a3842b1c00eb165f7e77cec
boldPro <probold777@gmail.com>
2024-03-04 15:47:15 -0500

    Sorting items to display in order of creation

--- lib/live_view_todo/item.ex
+++ lib/live_view_todo/item.ex
@@ -7,17 +7,17 @@ defmodule LiveViewTodo.Item do
   alias __MODULE__
 
   schema "items" do
+    field :person_id, :integer
     field :status, :integer, default: 0
     field :text, :string
-    # field :person_id, :integer
 
-    timestamps(type: :utc_datetime)
+    timestamps()
   end
 
   @doc false
   def changeset(item, attrs) do
     item
-    |> cast(attrs, [:text, :status])
+    |> cast(attrs, [:text, :person_id, :status])
     |> validate_required([:text])
   end
 
--- lib/live_view_todo_web/live/page_live.ex
+++ lib/live_view_todo_web/live/page_live.ex
@@ -3,15 +3,14 @@ defmodule LiveViewTodoWeb.PageLive do
   This module is implementation of PageLive in router
   """
   use LiveViewTodoWeb, :live_view
-
   alias LiveViewTodo.Item
 
 
-  @impl true
   @topic "live"
 
 
 
+  @impl true
   def mount(_params, _session, socket) do
     # For each LiveView in the root of a template, mount/3 is invoked twice: once to do the initial page load and again to establish the live socket.
     #After declaring the mount function we need to let the app know that we are going to use it
@@ -19,16 +18,27 @@ defmodule LiveViewTodoWeb.PageLive do
 
     # subscribe to the channel
     if connected?(socket), do: LiveViewTodoWeb.Endpoint.subscribe(@topic)
-    {:ok, assign(socket, items: Item.list_items(), editing: nil, tab: "all")}# socket is a liveView Socket that passes the endpoint for liveView todo web
+    #adding Items to assigns
+
+    {:ok, assign(socket, items: get_all_sorted_items(), editing: nil, tab: "all")}# socket is a liveView Socket that passes the endpoint for liveView todo web
   end
 
   @impl true
   def handle_event("create", %{"text" => text}, socket) do
     Item.create_item(%{text: text})
-    IO.puts("CREATING")
-    socket = assign(socket, items: Item.list_items(), active: %Item{})
+    socket = assign(socket, items: get_all_sorted_items(), active: %Item{})
     LiveViewTodoWeb.Endpoint.broadcast_from(self(), @topic, "update", socket.assigns)
-    IO.inspect(socket.items, label: "ITEMS in SOCKET")
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("toggle",data, socket) do
+    status = if Map.has_key?(data, "value"), do: 1, else: 0;
+    item = Item.get_item!(Map.get(data, "id"))
+
+    Item.update_item(item, %{id: item.id, status: status})
+    socket = assign(socket, items: get_all_sorted_items(), active: %Item{})
+    LiveViewTodoWeb.Endpoint.broadcast(@topic, "update", socket.assigns)
     {:noreply, socket}
   end
 
@@ -47,4 +57,9 @@ defmodule LiveViewTodoWeb.PageLive do
   def completed?(item) do
     if not is_nil(item.status) and item.status > 0, do: "completed", else: ""
   end
+
+  defp get_all_sorted_items() do
+    Enum.sort_by(Item.list_items(), &Map.fetch(&1, :inserted_at), :desc)
+  end
+
 end
