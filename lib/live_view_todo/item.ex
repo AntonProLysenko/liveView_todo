e8f039d65ad3984498c92b3e96e0d2749dd9d98c
boldPro <probold777@gmail.com>
2024-03-04 11:17:22 -0500

    added CRUD with tests

--- lib/live_view_todo/item.ex
+++ lib/live_view_todo/item.ex
@@ -3,11 +3,13 @@ defmodule LiveViewTodo.Item do
   # this command also creates the migration file for our db
   use Ecto.Schema
   import Ecto.Changeset
+  alias LiveViewTodo.Repo
+  alias __MODULE__
 
   schema "items" do
-    field :status, :integer
+    field :status, :integer, default: 0
     field :text, :string
-    field :person_id, :integer
+    # field :person_id, :integer
 
     timestamps(type: :utc_datetime)
   end
@@ -15,7 +17,78 @@ defmodule LiveViewTodo.Item do
   @doc false
   def changeset(item, attrs) do
     item
-    |> cast(attrs, [:text, :person_id, :status])
-    |> validate_required([:text, :person_id, :status])
+    |> cast(attrs, [:text, :status])
+    |> validate_required([:text])
   end
+
+
+
+
+
+
+  @doc """
+Creates a item.
+
+## Examples
+
+    iex> create_item(%{text: "Learn LiveView"})
+    {:ok, %Item{}}
+
+    iex> create_item(%{text: nil})
+    {:error, %Ecto.Changeset{}}
+
+"""
+def create_item(attrs \\ %{}) do
+  %Item{}
+  |> changeset(attrs)
+  |> Repo.insert()
+end
+
+@doc """
+Gets a single item.
+
+Raises `Ecto.NoResultsError` if the Item does not exist.
+
+## Examples
+
+    iex> get_item!(123)
+    %Item{}
+
+    iex> get_item!(456)
+    ** (Ecto.NoResultsError)
+
+"""
+def get_item!(id), do: Repo.get!(Item, id)
+
+
+@doc """
+Returns the list of items.
+
+## Examples
+
+    iex> list_items()
+    [%Item{}, ...]
+
+"""
+def list_items do
+  Repo.all(Item)
+end
+
+@doc """
+Updates a item.
+
+## Examples
+
+    iex> update_item(item, %{field: new_value})
+    {:ok, %Item{}}
+
+    iex> update_item(item, %{field: bad_value})
+    {:error, %Ecto.Changeset{}}
+
+"""
+def update_item(%Item{} = item, attrs) do
+  item
+  |> Item.changeset(attrs)
+  |> Repo.update()
+end
 end
